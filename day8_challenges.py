92beb9d08feabc22d839d5eafa132389078a4fb2
Mohammad Jakiur Rahman <rahman.mohammad2603@dutchbanglabank.com>
2025-07-18 12:27:22 +0600

    Second Commit

--- day8_challenges.py
+++ day8_challenges.py
@@ -4,12 +4,13 @@ while True:
     print("1. Add tasks")
     print("2. View all tasks")
     print("3. Mark tasks as done")
-    print("4. Exit")
+    print("4. Delete a task")
+    print("5. Exit")
 
     choice = input("Chose an option (1-4): ")
 
     if choice == "1":
-        task = input("Emter task: ")
+        task = input("Enter task: ")
         todo_list.append({"task" : task, "done" : False})
         print("Task Added Successfully")
     
@@ -33,6 +34,19 @@ while True:
             print("Invalid Task number")
     
     elif choice == "4":
+        if not todo_list:
+            print("The task list is empty")
+        else:
+            print("\n---Your TO DO List---")
+            del_task = int(input("Enter task number to delete the task: "))
+
+            if 1 <= del_task <= len(todo_list):
+                todo_list[del_task-1]["done"] = True
+                print("Task marked Done")
+            else:
+                print("Invalid Task number")
+    
+    elif choice == "5":
         print("Goodby")
         break
     else:
